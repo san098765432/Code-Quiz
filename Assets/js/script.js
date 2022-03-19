2c74ccd4c5b30f0d2f54e808beaf6c66fdd6741a
Sanyiah Zulfiqar <sanyiahzulfiqar1996@gmail.com>
2022-03-19 15:17:16 +0000

    adding questions using js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,9 +2,25 @@
 const start_btn = document.querySelector(".start_btn button");
 const info_box = document.querySelector(".info_box");
 const exit_btn = info_box.querySelector(".buttons .quit");
-const continue_btn = document.querySelector(".buttons .restart");
+const continue_btn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 
  //If start quiz button clicked
  start_btn.onclick = ()=>{
-     info_box.classList.add("activeInfo");
+     info_box.classList.add("activeInfo"); //show the info box 
  }
+
+
+//If exit  button button clicked
+ exit_btn.onclick = ()=>{
+     info_box.classList.remove("activeInfo"); //hide the info box
+ }
+
+ //If continue bbutton button clicked
+ continue_btn.onclick = ()=>{
+    info_box.classList.remove("activeInfo"); //hide the info box
+    quiz_box.classList.add("activeQuiz"); //show the quiz box
+}
+
+
+
